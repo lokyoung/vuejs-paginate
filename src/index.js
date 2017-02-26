cdebb3edebfc15728dcb63fafacae1dfc4ab3572
lokyoung <lokyoung@outlook.com>
2017-02-26 18:23:05 +0800

    refactor: change export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import Paginate from './components/Paginate'
 
-const vuejsPaginate = {}
-
-vuejsPaginate.install = (Vue) => {
-  Vue.component('paginate', Paginate)
-}
-
-module.exports = exports.default = vuejsPaginate
+module.exports = Paginate
